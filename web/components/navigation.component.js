1bf3c308c6873caabff2ae003787a23587fa1707
rextivlan <affan.ali386@gmail.com>
2021-03-25 22:04:19 +0530

    Add OTP Screen

--- web/components/navigation.component.js
+++ web/components/navigation.component.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { AuthScreen } from "./auth.component";
 import { DetailsScreen } from "./details.component";
+import { OTPScreen } from "./otp.component";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -10,6 +11,7 @@ const HomeNavigator = () => (
   <Navigator headerMode="none">
     <Screen name="Auth" component={AuthScreen} />
     <Screen name="Details" component={DetailsScreen} />
+    <Screen name="OTP" component={OTPScreen} />
   </Navigator>
 );
 
