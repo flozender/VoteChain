d486901f2441d19b719d345b33fc0c5d15a0ec70
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2021-04-17 14:48:00 +0530

    add email integration

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,10 @@
-let autho = require('../middleware/auth');
+let autho = require("../middleware/auth");
 module.exports = (app) => {
-  const voterController = require('../controllers/voter.js');
-  const adminController = require('../controllers/admin.js');
+  const voterController = require("../controllers/voter.js");
+  const adminController = require("../controllers/admin.js");
+  app.use("/", autho.adminTokenValidate);
 
-
-  app.post('/admin/auth', async (req, res) => {
+  app.post("/admin/auth", async (req, res) => {
     try {
       let body = Object.assign({}, req.body);
 
@@ -12,30 +12,28 @@ module.exports = (app) => {
       if (data && data.success) {
         res.status(200).send({
           success: true,
-          message: 'Logged In Successfully',
+          message: "Logged In Successfully",
           token: data.token,
           admin: {
             adminId: data.user.adminId,
             name: data.name,
             username: data.username,
-          }
-        })
+          },
+        });
       } else {
         res.status(200).send({
           success: false,
-          message: data.message
-        })
+          message: data.message,
+        });
       }
     } catch (error) {
       res.status(400).send({
-        error: JSON.stringify(error)
+        error: JSON.stringify(error),
       });
     }
-  })
+  });
 
-  app.use('/', autho.adminTokenValidate);
-
-  app.post('/admin/createVoter', async (req, res) => {
+  app.post("/admin/createVoter", async (req, res) => {
     try {
       let body = Object.assign({}, req.body);
 
@@ -43,61 +41,60 @@ module.exports = (app) => {
       if (data && data.success) {
         res.status(200).send({
           success: true,
-          message: 'Created Voter Successfully'
-        })
+          message: "Created Voter Successfully",
+        });
       } else {
         res.status(200).send({
           success: false,
-          message: data.message
-        })
+          message: data.message,
+        });
       }
     } catch (error) {
       res.status(400).send({
-        error: JSON.stringify(error)
+        error: JSON.stringify(error),
       });
     }
-  })
+  });
 
-  app.put('/admin/updateVoter/:voterId', async (req, res) => {
+  app.put("/admin/updateVoter/:voterId", async (req, res) => {
     try {
       let body = Object.assign({}, req.body);
 
       let data = await voterController.updateVoter(body, req.params.voterId);
       if (data && data.success) {
-        res.status(200).send(data)
+        res.status(200).send(data);
       } else {
         res.status(200).send({
           success: false,
-          message: data.message
-        })
+          message: data.message,
+        });
       }
     } catch (error) {
       res.status(400).send({
-        error: JSON.stringify(error)
+        error: JSON.stringify(error),
       });
     }
-  })
+  });
 
-  app.get('/admin/voters', async (req, res) => {
+  app.get("/admin/voters", async (req, res) => {
     try {
       let voters = await voterController.getAllVoters();
       res.status(200).send(voters);
     } catch (error) {
       res.status(400).send({
-        error: JSON.stringify(error)
+        error: JSON.stringify(error),
       });
     }
-  })
+  });
 
-  app.get('/admin/voter/:voterId', async (req, res) => {
+  app.get("/admin/voter/:voterId", async (req, res) => {
     try {
       let voter = await voterController.getVoter(req.params.voterId);
       res.status(200).send(voter);
     } catch (error) {
       res.status(400).send({
-        error: JSON.stringify(error)
+        error: JSON.stringify(error),
       });
     }
-  })
-
-}
+  });
+};
--- server/routes/voter.js
+++ server/routes/voter.js
@@ -1,8 +1,26 @@
-let autho = require('../middleware/auth');
-module.exports = (app) => {
-  const voterController = require('../controllers/voter.js');
+const fs = require("fs");
+const path = require("path");
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const handlebars = require("handlebars");
+const autho = require("../middleware/auth");
+const OAuth2 = google.auth.OAuth2;
 
-  app.post('/auth', async (req, res) => {
+const oauth2Client = new OAuth2(
+  process.env.OAuthClientID,
+  process.env.OAuthClientSecret,
+  "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.OAuthRefreshToken,
+});
+
+module.exports = (app) => {
+  const voterController = require("../controllers/voter.js");
+  app.use("/", autho.tokenValidate);
+
+  app.post("/auth", async (req, res) => {
     try {
       let body = Object.assign({}, req.body);
       body.id = body.voterId || null;
@@ -12,42 +30,85 @@ module.exports = (app) => {
       if (data && data.success) {
         res.send({
           success: true,
-          message: 'Logged In Successfully',
+          message: "Logged In Successfully",
           token: data.token,
           voter: {
             voterId: body.voterId,
             name: data.name,
-          }
-        })
+          },
+        });
       } else {
         res.send({
           success: false,
-          message: data.message
-        })
+          message: data.message,
+        });
       }
     } catch (error) {
       res.status(400).send({
-        error: JSON.stringify(error)
+        error: JSON.stringify(error),
       });
     }
-  })
+  });
 
-  app.post('/otp', (req, res) => {
+  app.post("/otp", (req, res) => {
     let body = Object.assign({}, req.body);
     body.id = body.voterId || null;
     body.dob = body.dob || null;
-  })
 
-  app.use('/', autho.tokenValidate);
+    const accessToken = oauth2Client.getAccessToken();
+    const smtpTransport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "votechain.iare@gmail.com",
+        clientId: process.env.OAuthClientID,
+        clientSecret: process.env.OAuthClientSecret,
+        refreshToken: process.env.OAuthRefreshToken,
+        accessToken: accessToken,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
 
-  app.get('/voter/:voterId', async (req, res) => {
+    const html = fs
+      .readFileSync(path.join(__dirname, "../templates/email.html"))
+      .toString();
+    const template = handlebars.compile(html);
+    const replacements = {
+      name: "VoteChain", // add voter's name here
+      otp: "203051", // add dynamic OTP here
+    };
+    const htmlToSend = template(replacements);
+
+    const mailOptions = {
+      from: "votechain.iare@gmail.com",
+      to: "votechain.iare@gmail.com", // Add the voter's email here
+      subject: "VoteChain Platform - OTP",
+      generateTextFromHTML: true,
+      html: htmlToSend,
+    };
+
+    smtpTransport.sendMail(mailOptions, (error) => {
+      if (error) {
+        console.log(error);
+      }
+      smtpTransport.close();
+      res.json({
+        success: true,
+        message: "Email sent",
+      });
+    });
+  });
+
+  app.get("/voter/:voterId", async (req, res) => {
     try {
       let voter = await voterController.getVoter(req.params.voterId);
       res.status(200).send(voter);
     } catch (error) {
       res.status(400).send({
-        error: JSON.stringify(error)
+        error: JSON.stringify(error),
       });
     }
-  })
-}
+  });
+};
