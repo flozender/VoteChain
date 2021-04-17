81019e9d3f2f7338c8c82aad05bf15bf242e3617
JethroGibbsV <boyapatikrishna99@gmail.com>
2021-04-17 17:14:08 +0530

    Models

--- server/models/electionRepo.js
+++ server/models/electionRepo.js
@@ -44,22 +44,4 @@ module.exports = {
       });
   },
 
-  // getAll: function (manager_id, is_admin, space_ids) {
-  //   let condition = (!is_admin) ? `WHERE T.space_id in (:space_ids)` : ``;
-  //   let query = `SELECT T.id, T.name, T.price, T.is_active,
-  //    T.capacity, T.space_id,
-  //    CONCAT(T.capacity, ' ', 'Seater Cabin') as name,
-  //    CONCAT(REPLACE(S.location_name, ' ', '-'), '-', S.id) as link_name
-  //    FROM election T
-  //    LEFT JOIN
-  //    spaces S
-  //    ON S.id = T.space_id
-  //    ${condition}`;
-
-  //   return db.query(query, { replacements: { space_ids }, type: db.QueryTypes.SELECT })
-  //     .then(data => data)
-  //     .catch(error => {
-  //       throw error;
-  //     })
-  // }
 }
