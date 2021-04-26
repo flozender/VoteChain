c024e7873d0af8eeea22b03a157dbb29a290572f
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2021-04-26 17:31:58 +0530

    Add solidity tests

--- ethereum/contracts/Voting.sol
+++ ethereum/contracts/Voting.sol
@@ -3,9 +3,17 @@ pragma solidity ^0.5.16;
 contract Voting {
 
     event AddedElection(uint electionID);
-    event AddedPartyElection(uint partyElectionID);
-    event AddedCandidateElection(uint candidateElectionID);
-    event VoteCasted(uint voterElectionID);
+    event AddedPartyElection(uint partyID);
+    event PartyAlreadyExists(uint partyID);
+    event AddedCandidateElection(uint candidateID);
+    event VoteCasted(string voterID);
+    event AlreadyVoted(string voterID);
+    event ElectionNotFound(uint electionID);
+    event PartyNotFound(uint partyID);
+    event CandidateNotFound(uint candidateID);
+    event ElectionAlreadyExists(uint electionID);
+    event PartyAlreadyStandingForRegion(uint partyID);
+    event CandidateAlreadyStandingForElection(uint candidateID);
 
     address owner;
     constructor() public {
@@ -23,7 +31,7 @@ contract Voting {
     
     struct VoterElection {
         uint id;
-        bytes32 voterID;
+        string voterID;
         uint regionID;
         uint electionID;
         uint candidateID;
@@ -62,51 +70,84 @@ contract Voting {
      * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
      
     function addElection(uint id) onlyOwner public {
-        require(elections[id].doesExist == false);
-        numElections++;
-        elections[id] = Election(id, true);
-        emit AddedElection(id);
+        if (elections[id].doesExist == false) {
+            numElections++;
+            elections[id] = Election(id, true);
+            emit AddedElection(id);
+        } else {
+            emit ElectionAlreadyExists(id);
+        }
     }
 
     function addParty(uint partyID, uint electionID) onlyOwner public {
         int currentPartyID = getPartyID(partyID, electionID);
-        if (currentPartyID != -1) {
+        if (currentPartyID == -1) {
             uint partyElectionID = numPartyElections++;
             partyElections[partyElectionID] = PartyElection(partyElectionID, partyID, electionID, 0, true);
-            emit AddedPartyElection(partyElectionID);
+            emit AddedPartyElection(partyID);
         }
+            emit PartyAlreadyExists(partyID);
     }
 
     function addCandidateElection(uint candidateID, uint electionID, uint regionID, uint partyID) onlyOwner public {
-        uint candidateElectionID = numCandidateElections++;
-        candidateElections[candidateElectionID] = CandidateElection(candidateElectionID, candidateID, partyID, regionID, electionID, 0, true);
-        emit AddedCandidateElection(candidateElectionID);
+        int partyRegionID = hasPartyStoodForRegion(partyID, electionID, regionID);
+        if (partyRegionID == -1) {
+            bool hasStood = hasCandidateStoodForElection(candidateID, electionID);
+            if (hasStood == false){
+                uint candidateElectionID = numCandidateElections++;
+                candidateElections[candidateElectionID] = CandidateElection(candidateElectionID, candidateID, partyID, regionID, electionID, 0, true);
+                emit AddedCandidateElection(candidateID);
+            } else {
+                emit CandidateAlreadyStandingForElection(candidateID);
+                revert("Candidate Is Already Standing For Election");
+            }
+        } else {
+            emit PartyAlreadyStandingForRegion(partyID);
+            revert("Party Is Already Standing For Region");
+
+        }
     }
 
-    function vote(bytes32 voterID, uint electionID, uint regionID, uint candidateID) onlyOwner public {
-        require(elections[electionID].doesExist == true);
-        int currentCandidateID = getCandidateID(candidateID, electionID, regionID);
-        require(currentCandidateID != -1);
-        bool hasVoted = hasVoterVoted(voterID, electionID);
-        require(hasVoted == false);
-        uint voterElectionID = numVoterElections++;
-        voterElections[voterElectionID] = VoterElection(voterElectionID, voterID, regionID, electionID, candidateID);
-        candidateElections[uint(currentCandidateID)].votes++;
-        uint partyID = candidateElections[uint(currentCandidateID)].partyID;
-        int currentPartyID = getPartyID(partyID, electionID);
-        require(currentPartyID != -1);
-        partyElections[uint(currentPartyID)].votes++;
-        emit VoteCasted(voterElectionID);
+    function vote(string memory voterID, uint electionID, uint regionID, uint candidateID) onlyOwner public {
+        if (elections[electionID].doesExist == true) {
+            int currentCandidateID = getCandidateID(candidateID, electionID, regionID);
+            if (currentCandidateID != -1) {
+                bool hasVoted = hasVoterVoted(voterID, electionID);
+                if (hasVoted == false) {
+                    uint voterElectionID = numVoterElections++;
+                    voterElections[voterElectionID] = VoterElection(voterElectionID, voterID, regionID, electionID, candidateID);
+                    candidateElections[uint(currentCandidateID)].votes++;
+                    uint partyID = candidateElections[uint(currentCandidateID)].partyID;
+                    int currentPartyID = getPartyID(partyID, electionID);
+                    if (currentPartyID != -1) {
+                        partyElections[uint(currentPartyID)].votes++;
+                        emit VoteCasted(voterID);
+                    } else {
+                        emit PartyNotFound(partyID);
+                        revert("Party Not Found");
+                    }
+                } else {
+                    emit AlreadyVoted(voterID);
+                    revert("Already Voted");
+                }
+            } else {
+                emit CandidateNotFound(candidateID);
+                revert("Candidate Not Found");
+            }
+        } else {
+            emit ElectionNotFound(electionID);
+            revert("Election Not Found");
+        }
     }
 
     /* * * * * * * * * * * * * * * * * * * * * * * * * * 
      *  Getter Functions, marked by the key word "view" *
      * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-    function hasVoterVoted(bytes32 voterID, uint electionID) private view returns(bool) {
+    function hasVoterVoted(string memory voterID, uint electionID) private view returns(bool) {
         bool voted = false;
         for (uint i = 0; i < numVoterElections; i++){
-            if (voterElections[i].voterID == voterID && voterElections[i].electionID == electionID){
+            if (keccak256(bytes(voterElections[i].voterID)) == keccak256(bytes(voterID)) && voterElections[i].electionID == electionID) {
                 voted = true;
                 break;
             }
@@ -114,8 +155,31 @@ contract Voting {
         return voted;
     }
 
+    function hasCandidateStoodForElection(uint candidateID, uint electionID) public view returns(bool) {
+        for (uint i = 0; i < numCandidateElections; i++) {
+            if (candidateElections[i].candidateID == candidateID && candidateElections[i].electionID == electionID) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     function getNumOfElections() public view returns(uint) {
         return numElections;
+    }
+
+    function getCountOfParties() public view returns(uint) {
+        return numPartyElections;
+    }
+
+    function getNumOfParties(uint electionID) public view returns(uint) {
+        uint count = 0;
+        for (uint i = 0; i < numPartyElections; i++) {
+            if (partyElections[i].electionID == electionID) {
+                count++;
+            }
+        }
+        return count;
     }
 
     function getNumOfCandidates(uint electionID) public view returns(uint) {
@@ -138,7 +202,7 @@ contract Voting {
         return count;
     }
 
-    function getCandidateID(uint candidateID, uint electionID, uint regionID) private view returns(int) {
+    function getCandidateID(uint candidateID, uint electionID, uint regionID) public view returns(int) {
         int currentCandidateID = -1;
         for (uint i = 0; i < numCandidateElections; i++) {
             if (candidateElections[i].candidateID == candidateID && candidateElections[i].electionID == electionID && candidateElections[i].regionID == regionID) {
@@ -149,10 +213,21 @@ contract Voting {
         return currentCandidateID;
     }
 
-    function getPartyID(uint partyID, uint electionID) private view returns(int) {
+    function getPartyID(uint partyID, uint electionID) public view returns(int) {
         int currentPartyID = -1;
         for (uint i = 0; i < numPartyElections; i++) {
             if (partyElections[i].partyID == partyID && partyElections[i].electionID == electionID) {
+                currentPartyID = int(i);
+                break;
+            }
+        }
+        return currentPartyID;
+    }
+
+    function hasPartyStoodForRegion(uint partyID, uint electionID, uint regionID) public view returns(int) {
+        int currentPartyID = -1;
+        for (uint i = 0; i < numCandidateElections; i++) {
+            if (candidateElections[i].partyID == partyID && candidateElections[i].electionID == electionID && candidateElections[i].regionID == regionID) {
                 currentPartyID = int(i);
                 break;
             }
--- ethereum/test/TestVoting.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.18;
-
-import "truffle/Assert.sol";
-import "truffle/DeployedAddresses.sol";
-import "../contracts/Voting.sol";
-
